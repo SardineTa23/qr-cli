73abae39a7f749a0d2a53d0521bb4c640a481883
SardineTa23 <wonderland.7878@gmail.com>
2022-05-01 18:26:21 +0900

    コマンドの引数をファイルに書き込む

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	flag.Parse()
 	arg := flag.Arg(0)
-	fmt.Printf("Hello %s\n", arg)
+	msg := fmt.Sprintf("Hello %s\n", arg)
+
+	f, err := os.Create("./hello.txt")
+	if err != nil {
+		fmt.Printf("failed to create file \n: %v", err)
+		return
+	}
+	defer f.Close()
+
+	// ファイルに書き込み
+	_, err = f.WriteString(msg)
+	if err != nil {
+		fmt.Printf("failed to write message to file \n: %v", err)
+	}
+
+	fmt.Println("Done!")
 }
